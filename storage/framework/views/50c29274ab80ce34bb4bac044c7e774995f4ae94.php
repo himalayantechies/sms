192a381c1af470db1c7be037403546854be39c30
badri <badri305@gmail.com>
2023-01-27 12:36:21 +0545

    Changes

---
+++ storage/framework/views/50c29274ab80ce34bb4bac044c7e774995f4ae94.php
@@ -0,0 +1,10 @@
+<!-- Favicons -->
+<link rel="icon" href="<?php echo e(asset('public/frontend/assets/image/purple_gradient.png')); ?>">
+<!-- Fontawasome Css -->
+<link rel="stylesheet" href="<?php echo e(asset('public/frontend/assets/css/all.min.css')); ?>">
+<!-- Bootstarp Css -->
+<link rel="stylesheet" href="<?php echo e(asset('public/frontend/assets/css/bootstrap.min.css')); ?>">
+<!-- Main Css -->
+<link rel="stylesheet" href="<?php echo e(asset('public/frontend/assets/css/style.css')); ?>">
+<!-- Responsive Css -->
+<link rel="stylesheet" href="<?php echo e(asset('public/frontend/assets/css/responsive.css')); ?>"><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/sms/resources/views/frontend/include_top.blade.php ENDPATH**/ ?>
