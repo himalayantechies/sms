9b97c531e030a148e4117fc237329adc9f0a0074
bimal-tech <074bct107@nce.edu.np>
2023-02-01 12:35:57 +0545

    Login of user completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -45,8 +46,8 @@ class User extends Authenticatable
     }
 
 
-     public function getTomoNameAttribute()
-    {
-        return $this->checkEnrollment()->class_id;  
-    }
+    //  public function getTomoNameAttribute()
+    // {
+    //     return $this->checkEnrollment()->class_id;  
+    // }
 }
