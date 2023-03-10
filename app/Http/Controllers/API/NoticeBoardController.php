84635518bc5b45e9ea0f20fbd87c730ed047677a
badri <badri305@gmail.com>
2023-03-10 11:33:31 +0545

    Added imagepath on noticeboard api

--- app/Http/Controllers/API/NoticeBoardController.php
+++ app/Http/Controllers/API/NoticeBoardController.php
@@ -14,7 +14,12 @@ class NoticeBoardController extends Controller
             $school_id = auth()->user()->school_id;
             
             $notices = (new Noticeboard)->get_all_Notice($active_session, $school_id);
-            return response()->json(["success" => true, "data" => $notices, "msg" => "Notices returned successfully"]);
+            return response()->json([
+                                        "success" => true, 
+                                        "data" => $notices, 
+                                        "msg" => "Notices returned successfully",
+                                        "imagepath" => "/public/assets/uploads/noticeboard/"
+                                    ]);
 
         }catch(Exception $exp){
             return response()->json(["success" => false, "msg" => $exp->getMessage()]);
